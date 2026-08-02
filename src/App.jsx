ca5bf81bc13b94bab5c7ab90bc1fa736064e4acd
SnezzyDwarf <bruno.pernao@essp.pt>
2026-08-02 12:04:56 +0100

    chore: set up project structure and React Router

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+
+//Layouts
+import MainLayout from "./layouts/MainLayout";
+
+//Pages
+import Game from "./pages/Game/Game";
+import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route element={<MainLayout />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/game" element={<Game />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  );
+}
